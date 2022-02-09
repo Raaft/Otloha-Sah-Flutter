82fa49ca002a25ca08130305d038e5f4b642984e
MohammedIbraheemZaki <zakiz0054@gmail.com>
2022-02-09 21:21:24 +0200

    enhance FORGET PASSWORD SCREEN

--- lib/core/utils/res/icons_app.dart
+++ lib/core/utils/res/icons_app.dart
@@ -6,6 +6,7 @@ class AppIcons {
   static String homeIcon = 'assets/icons/home.png';
   static String searchIcon = 'assets/icons/search.png';
   static String quranIcon = 'assets/icons/quran.png';
+  static String quran4Icon = 'assets/icons/quran4.png';
   static String quran2Icon = 'assets/icons/quran2.png';
   static String quran3Icon = 'assets/icons/quran3.png';
   static String profileIcon = 'assets/icons/profile.png';
