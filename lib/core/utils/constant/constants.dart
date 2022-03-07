d0e517d541fd0374bbe37f1a154849dc827e292c
Raaft <raaftegyez1@gmail.com>
2022-03-07 20:12:22 +0200

    update auth

--- lib/core/utils/constant/constants.dart
+++ lib/core/utils/constant/constants.dart
@@ -4,8 +4,8 @@ const String fontFamily2 = 'ElMessiri';
 var isEn = false;
 
 //const String clientId = 'NIniFeO60oKPRpNbZYeW8w4wcGx6F1Ni48s59JmV';
-const String clientId = 'rVkbqZn6wMirBHQw5oHlBtjhVva3YUyRtqMkCpMV';
+const String clientId = 'RQPtGX8gXJBWLrftNpc6GZr8hGNm6bR4QwI9XDQy';
 
 //const String clientSecret = '7VJuQ1DrwQzbMilum88be5tLCKx3QJOZ1Rjzp3dUTwNfo2yB8ir9qLWb5Y2xRGnfKcftZxkpVPflXaeP7KjteRFIZREp8WwOcr8Trvq5WVml84DKHoIHXjktBrzy3utk';
 const String clientSecret =
-    'J7nAjiVErez2eKESRV41KccwgTc9rQNokxyCmQdQrxk13cHyGOp6YhDDcd0HJSkbuYgs28xc0l5Jd6CTVmCmZjtcx3ZT45vhbYiL9KL06caFGjGRSH1S0vjf79qHtu8z';
+    'RKsbz1PbmyAhXJSUOdduOJaczDtsaauG3DgeX1I79ZTOJyv7hxXxLq2yk8y7EZGC61PDRK21xOln2kmu7UnByJaDeAR9e8gCB9AZ3KN4OpwOCZUwalZiNU1FHDemkwrL';
