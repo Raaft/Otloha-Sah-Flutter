199903b0c9d58dacd445a296b7e67395b046721b
ShadySamirBakheet <shadysamirbakheet@gmail.com>
2022-03-07 12:25:50 +0200

    add add edits  to Repo at 12 PM on 7 mar 2022

--- lib/modules/settings/business_logic/chapter/chapter_cubit.dart
+++ lib/modules/settings/business_logic/chapter/chapter_cubit.dart
@@ -5,6 +5,8 @@ import 'package:quran_widget_flutter/quran_widget_flutter.dart';
 part 'chapter_state.dart';
 
 class ChapterCubit extends Cubit<ChapterState> {
+  Recitation? recitation;
+
   ChapterCubit() : super(ChapterInitial());
 
   fetchChaptersList({String? qurey, bool isSelect = false}) {
@@ -15,8 +17,20 @@ class ChapterCubit extends Cubit<ChapterState> {
       if (isSelect || (narrationId != null && reciterId != null)) {
         DataSource.instance.fetchChaptersList(qurey: qurey).then((value) async {
           if (value!.isNotEmpty) {
-            emit(ChapterInitial());
-            emit(ChapterFetched(value));
+            DataSource.instance
+                .fetchRecitationsList(
+                    reciterId: reciterId, narrationId: narrationId)
+                .then((recitations) {
+              recitation = recitations!.firstWhere((element) =>
+                  (element.narrationId == narrationId &&
+                      element.reciterId == reciterId));
+              if (recitation != null) {
+                emit(ChapterInitial());
+                emit(ChapterFetched(value));
+              } else {
+                emit(const ChapterError('Not Found Data'));
+              }
+            });
           } else {
             emit(const ChapterError('Not Found Data'));
           }
