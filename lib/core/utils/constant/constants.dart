d8a0d001daf1aae2d76da48436961f9690a9a6bd
Raaft <raaftegyez1@gmail.com>
2022-03-08 16:34:01 +0200

    secrt

--- lib/core/utils/constant/constants.dart
+++ lib/core/utils/constant/constants.dart
@@ -5,7 +5,7 @@ var isEn = false;
 
 //const String clientId = 'NIniFeO60oKPRpNbZYeW8w4wcGx6F1Ni48s59JmV';
 const String clientId = 'RQPtGX8gXJBWLrftNpc6GZr8hGNm6bR4QwI9XDQy';
-
+///////
 //const String clientSecret = '7VJuQ1DrwQzbMilum88be5tLCKx3QJOZ1Rjzp3dUTwNfo2yB8ir9qLWb5Y2xRGnfKcftZxkpVPflXaeP7KjteRFIZREp8WwOcr8Trvq5WVml84DKHoIHXjktBrzy3utk';
 const String clientSecret =
     'RKsbz1PbmyAhXJSUOdduOJaczDtsaauG3DgeX1I79ZTOJyv7hxXxLq2yk8y7EZGC61PDRK21xOln2kmu7UnByJaDeAR9e8gCB9AZ3KN4OpwOCZUwalZiNU1FHDemkwrL';
