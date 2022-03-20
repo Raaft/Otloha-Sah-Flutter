93c4fea45f34e509ef34a53c7c67b5a7d78d0b48
Raaft <raaftegyez1@gmail.com>
2022-03-20 23:13:41 +0200

    fix aut sign up

--- lib/core/utils/constant/constants.dart
+++ lib/core/utils/constant/constants.dart
@@ -12,11 +12,11 @@ bool isLogin = false;
 
 var isEn = false;
 
-const String clientId = '8qooczKuIdgU1ES0xfc0pEYEqPJRytXHxBcyY7TT';
+const String clientId = 'e6tY9LQEUxH38LmYXgCBPsTbKhjgjZhoFOivawjC';
 ///////
 //const String clientSecret = '7VJuQ1DrwQzbMilum88be5tLCKx3QJOZ1Rjzp3dUTwNfo2yB8ir9qLWb5Y2xRGnfKcftZxkpVPflXaeP7KjteRFIZREp8WwOcr8Trvq5WVml84DKHoIHXjktBrzy3utk';
 const String clientSecret =
-    'O2JDrwEaQ0mQQLC2fjNyGSxTQ2qkjmfTfad4qKZn2Ut0P1kaHnT0x55pQd59c18t9FiKjok0chjvA6DvegslL2T1xL0ETqC5nRZ0WXNWWqF2uNTFnJHBOgaTHngIvHVi';
+    'w7ASoyLo5gdoDQxZ1LGQ704Ys724rDYK9Dsj3Xa4FvkUKpCQ6FW7u0KClToWV6HtKChWHXgF7vegQABRJZOo1enqj5xW4Y50FlDHQ1hmiAXNJMCBx94tqWYxQvCjPEPT';
 
 const String languagesSelectedName = 'LanguagesSelectedName';
 const String languagesSelectedId = 'LanguagesSelectedId';
